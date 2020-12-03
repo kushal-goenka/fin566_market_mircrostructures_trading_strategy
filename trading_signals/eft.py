3d22ba75f52b26be8b3a4377ff29c87d580b5b7c
Yong Xie <yongxie2@illinois.edu>
2020-12-02 21:24:23 -0500

    add function signiture

---
+++ trading_signals/eft.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+@file: eft
+@author: Yong Xie
+@time: 12/2/2020 11:43 PM
+@Description: 
+
+This script is used to contain utility function involved with eft.
+
+"""
+
+
+def calc_theoretical_eft_price(folders, dates, tickers, weights):
+    """[summary]
+
+    Args:
+        folders ([str]): [folder of order data]
+        dates ([list of str]): [date]
+        tickers ([list]): [tickers for components]
+        weights ([list]): [list of component weights corresponding to tickers]
+    
+    Returns:
+        Price process for each order update
+    """
+
+    pass
+
+    
