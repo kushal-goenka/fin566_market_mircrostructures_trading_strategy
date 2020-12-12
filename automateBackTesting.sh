06815110570f693512bad9858d0eebef8f6010ca
Students Name <student@illinois.edu>
2020-12-12 00:15:09 +0000

    Cleaned up automation script with comments

--- automateBackTesting.sh
+++ automateBackTesting.sh
@@ -3,43 +3,59 @@ strategyName="Team3Strategy"
 startDate="2019-10-29"
 endDate="2019-10-30"
 sharedObjectFileName="Team3Strategy.so"
+
 strategyFolderPath="/home/vagrant/Desktop/strategy_studio/localdev/RCM/StrategyStudio/examples/fin566_fall_2020_group_three/Team3Strategy/"
-sharedObjectOriginPath="/home/vagrant/Desktop/strategy_studio/localdev/RCM/StrategyStudio/examples/fin566_fall_2020_group_three/Team3Strategy/$sharedObjectFileName"
+sharedObjectOriginPath="$strategyFolderPath/$sharedObjectFileName"
 sharedObjectDestinationPath="/home/vagrant/Desktop/strategy_studio/backtesting/strategies_dlls"
 
-# /home/vagrant/Desktop/strategy_studio/backtesting/./StrategyServerBacktesting&
 
+# Remake the .so files after making modifications to the source code
 (cd $strategyFolderPath && make clean && make all)
-
+# Copy .so file from Strategy folder to the destination folder
 cp $sharedObjectOriginPath $sharedObjectDestinationPath
 
+# Run BackTesting Server in the background
 (cd /home/vagrant/Desktop/strategy_studio/backtesting && ./StrategyServerBacktesting&)
 
+# Create the Instance of the given strategy, might throw errors if it exists, but thats okay
 /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd create_instance $instanceName $strategyName UIUC SIM-1001-101 dlariviere 1000000 -symbols SPY
 
+# Get the current number of lines in the main_log.txt file
 logFileNumLines=$(wc -l < /home/vagrant/Desktop/strategy_studio/backtesting/logs/main_log.txt)
-echo "Number of lines Currently in Log file:",$logFileNumLines
 
+# DEBUGGING OUTPUT
+# echo "Number of lines Currently in Log file:",$logFileNumLines
+
+# Start the backtest
 /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd start_backtest $startDate $endDate $instanceName 1
 
+# Get the line number which ends with finished. 
 foundFinishedLogFile=$(grep -nr "finished.$" /home/vagrant/Desktop/strategy_studio/backtesting/logs/main_log.txt | gawk '{print $1}' FS=":"|tail -1)
-echo "Last line found:",$foundFinishedLogFile
 
+# DEBUGGING OUTPUT
+# echo "Last line found:",$foundFinishedLogFile
+
+# If the line ending with finished. is less than the previous length of the log file, then strategyBacktesting has not finished, 
+# once its greater than the previous, it means it has finished.
 while ((logFileNumLines > foundFinishedLogFile))
 do
+    foundFinishedLogFile=$(grep -nr "finished.$" /home/vagrant/Desktop/strategy_studio/backtesting/logs/main_log.txt | gawk '{print $1}' FS=":"|tail -1)
 
-foundFinishedLogFile=$(grep -nr "finished.$" /home/vagrant/Desktop/strategy_studio/backtesting/logs/main_log.txt | gawk '{print $1}' FS=":"|tail -1)
-echo "HERE"
+    #DEBUGGING OUTPUT
+    # echo "HERE"
 done
 
-echo "Last line found after ending:",$foundFinishedLogFile
+# DEBUGGING OUTPUT
+# echo "Last line found after ending:",$foundFinishedLogFile
 
-# /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd start_backtest 2019-10-30 2019-10-30 $instanceName 1
+# Get the latest file in terms of modified time
 latestCRA=$(ls -t /home/vagrant/Desktop/strategy_studio/backtesting/backtesting-results | head -1)
-echo "latest CRA File is: $latestCRA"
+
+#DEBUGGING OUTPUT
+# echo "latest CRA File is: $latestCRA"
+
+# Run the export command to generate csv files
 /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd export_cra_file backtesting-results/$latestCRA backtesting-cra-exports
 
+# Quit strategy studio working in the background
 /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd quit
-
-# logFileNumLines=$(wc -l < /home/vagrant/Desktop/strategy_studio/backtesting/logs/main_log.txt)
-# echo "Number of lines:",$logFileNumLines
