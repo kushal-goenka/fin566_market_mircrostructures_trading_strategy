07c1d6d157faf3d31a97926ae3451dc474471bcb
Students Name <student@illinois.edu>
2020-12-12 00:03:30 +0000

    Working bash script without the need to run make clean or make

--- automateBackTesting.sh
+++ automateBackTesting.sh
@@ -1,15 +1,19 @@
-instanceName="S4"
-strategyName="SimpleTrade"
+instanceName="S5"
+strategyName="Team3Strategy"
 startDate="2019-10-29"
 endDate="2019-10-30"
-sharedObjectFileName="SimpleTradeStrategy.so"
-sharedObjectOriginPath="/home/vagrant/Desktop/strategy_studio/localdev/RCM/StrategyStudio/examples/strategies/SimpleTradeStrategy/$sharedObjectFileName"
+sharedObjectFileName="Team3Strategy.so"
+strategyFolderPath="/home/vagrant/Desktop/strategy_studio/localdev/RCM/StrategyStudio/examples/fin566_fall_2020_group_three/Team3Strategy/"
+sharedObjectOriginPath="/home/vagrant/Desktop/strategy_studio/localdev/RCM/StrategyStudio/examples/fin566_fall_2020_group_three/Team3Strategy/$sharedObjectFileName"
 sharedObjectDestinationPath="/home/vagrant/Desktop/strategy_studio/backtesting/strategies_dlls"
 
-# cp $sharedObjectOriginPath $sharedObjectDestinationPath
 # /home/vagrant/Desktop/strategy_studio/backtesting/./StrategyServerBacktesting&
 
-# (cd /home/vagrant/Desktop/strategy_studio/backtesting && ./StrategyServerBacktesting&)
+(cd $strategyFolderPath && make clean && make all)
+
+cp $sharedObjectOriginPath $sharedObjectDestinationPath
+
+(cd /home/vagrant/Desktop/strategy_studio/backtesting && ./StrategyServerBacktesting&)
 
 /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd create_instance $instanceName $strategyName UIUC SIM-1001-101 dlariviere 1000000 -symbols SPY
 
@@ -25,7 +29,7 @@ while ((logFileNumLines > foundFinishedLogFile))
 do
 
 foundFinishedLogFile=$(grep -nr "finished.$" /home/vagrant/Desktop/strategy_studio/backtesting/logs/main_log.txt | gawk '{print $1}' FS=":"|tail -1)
-
+echo "HERE"
 done
 
 echo "Last line found after ending:",$foundFinishedLogFile
@@ -35,7 +39,7 @@ latestCRA=$(ls -t /home/vagrant/Desktop/strategy_studio/backtesting/backtesting-
 echo "latest CRA File is: $latestCRA"
 /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd export_cra_file backtesting-results/$latestCRA backtesting-cra-exports
 
-# /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd quit
+/home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd quit
 
 # logFileNumLines=$(wc -l < /home/vagrant/Desktop/strategy_studio/backtesting/logs/main_log.txt)
 # echo "Number of lines:",$logFileNumLines
