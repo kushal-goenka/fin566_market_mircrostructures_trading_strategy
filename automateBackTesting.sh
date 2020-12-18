9685477f1b8376050ddd4ad9dc91bc82aa7c08b2
Yong Xie <yongxie2@illinois.edu>
2020-12-17 20:04:49 -0500

    add calc metric to the workflow

--- automateBackTesting.sh
+++ automateBackTesting.sh
@@ -68,3 +68,6 @@ latestCRA=$(ls -t /home/vagrant/Desktop/strategy_studio/backtesting/backtesting-
 
 # Quit strategy studio working in the background
 /home/vagrant/Desktop/strategy_studio/backtesting/utilities/./StrategyCommandLine cmd quit
+
+# Generate portfolio metric
+python /home/vagrant/Desktop/strategy_studio/localdev/RCM/StrategyStudio/examples/fin566_fall_2020_group_three/metrics/calc_metric.py 
